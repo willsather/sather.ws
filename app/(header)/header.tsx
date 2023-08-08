f6cb0d78a0e7f19c83b0f63fcd8ec4cbdcfdc88f
Will Sather <satherw@vmware.com>
2023-08-07 22:59:45 -0500

    feat: add dynamic header

--- app/(header)/header.tsx
+++ app/(header)/header.tsx
@@ -1,32 +1,39 @@
 "use client";
 
-import Image from "next/image";
-import { Box } from "@mui/material";
-import HeaderMenuItem from "@/app/(header)/MenuItem";
+import { Box, Typography } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/src/theme/theme";
+import Link from "next/link";
+import Image from "next/image";
+import menuLinks from "@/app/(header)/menuLinks";
+import styles from "@/app/(header)/header.module.scss";
 
 const Header = () => {
-  const menuLinks = {
-    home: "/",
-    experience: "/experience",
-    blog: "https://blog.sather.ws",
-    // byt8: "https://byt8.app",
-    about: "/about",
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ display: "flex", justifyContent: "space-between", position: "sticky" }} margin={4}>
         <Image src="/assets/logo.png" alt="Will Sather" width={100} height={100} />
 
-        <Box>
-          <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-            {Object.entries(menuLinks).map(([text, link]) => (
-              <HeaderMenuItem text={text} link={link} key={text} />
-            ))}
-          </Box>
+        <Box sx={{ display: "flex", flexDirection: { xs: "column", md: "row" }, justifyContent: "space-between" }}>
+          {Object.entries(menuLinks).map(([text, link]) => (
+            <Box margin={{ md: "15px" }} key={text}>
+              <Link
+                className={styles.headerMenuItem}
+                href={link}
+                target="_self"
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  textAlign: "right",
+                }}
+              >
+                <Typography variant="h4">{text}</Typography>
+              </Link>
+            </Box>
+          ))}
         </Box>
+
+        {/*<ThemeSwitch />*/}
       </Box>
     </ThemeProvider>
   );
---
+++ app/(header)/menuLinks.ts
@@ -0,0 +1,9 @@
+const menuLinks = {
+  home: "/",
+  experience: "/experience",
+  blog: "https://blog.sather.ws",
+  // byt8: "https://byt8.app",
+  about: "/about",
+};
+
+export default menuLinks;
