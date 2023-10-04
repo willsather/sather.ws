b5253db7085506995529537eb64c43cec6f69cf2
Will Sather <satherw@vmware.com>
2023-10-04 08:49:33 -0700

    chore: refactor and update open-graph

--- src/app/blog/posts/[slug]/opengraph-image.tsx
+++ src/app/blog/posts/[slug]/opengraph-image.tsx
@@ -1,25 +1,25 @@
-/* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from "next/server";
 import { loadMdxFromSlug } from "@/lib/blog/utils";
 import { BlogFrontMatter } from "@/src/types/blogFrontMatter";
-import LogoIcon from "@/src/app/blog/posts/[slug]/logo";
+import Logo from "@/src/components/shared/logo";
 
 export const size = {
   width: 1200,
   height: 630,
 };
-export const alt = "Expolorer | Blog";
+
+export const alt = "Blog | Will Sather";
 export const contentType = "image/png";
 
 export default async function og({ params }: { params: { slug: string } }) {
-  const slug = params.slug;
   const { data } = await loadMdxFromSlug(params?.slug);
   const frontMatter = data as BlogFrontMatter;
+
   return new ImageResponse(
     (
       <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
         <div tw="flex flex-none items-center justify-center h-[160px] w-[160px]">
-          <LogoIcon width="64" height="58" fill="white" />
+          <Logo width="64" height="58" fill="white" />
         </div>
         <div tw="flex items-center justify-center mt-12 mx-16">
           <h1 tw="text-7xl text-center font-bold text-white font-sans font-bold">{frontMatter.title}</h1>
