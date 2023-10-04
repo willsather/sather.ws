e4a1424ae3782993130367a9779f9266666a9c8b
Will Sather <satherw@vmware.com>
2023-10-04 00:27:38 -0700

    fix: case issues for mui

--- src/components/blog/CustomLink.tsx
+++ src/components/blog/CustomLink.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "@mui/material/link";
+import Link from "@mui/material/Link";
 import { styled } from "@mui/material/styles";
 import type { LinkProps as MuiLinkProps } from "@mui/material";
 
