30b524833a8a42bddbc92e2a08773b35eaf42bd2
Will Sather <satherw@vmware.com>
2023-10-17 21:04:33 -0400

    chore: cleanup navbar

--- src/app/(header)/navigationBar.tsx
+++ src/app/(header)/navigationBar.tsx
@@ -7,39 +7,6 @@ import React, { useState } from "react";
 import MenuIcon from "@/public/icons/menu.svg";
 import CloseIcon from "@/public/icons/close.svg";
 
-const Links = () => {
-  return (
-    <>
-      {Object.entries(menuLinks).map(([text, link]) => (
-        <Box margin={{ md: 2 }} marginTop={{ xs: 2, md: 0 }} key={text}>
-          <Box
-            sx={[
-              {
-                "&:hover": {
-                  textDecoration: "underline",
-                },
-              },
-            ]}
-            onClick={() => setM}
-          >
-            <Link
-              href={link}
-              target="_self"
-              style={{
-                color: "black",
-                textDecoration: "none",
-                textAlign: "right",
-              }}
-            >
-              <Typography variant="body1">{text}</Typography>
-            </Link>
-          </Box>
-        </Box>
-      ))}
-    </>
-  );
-};
-
 const NavigationBar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
 
@@ -48,7 +15,7 @@ const NavigationBar = () => {
   return (
     <Stack flexDirection={{ xs: "column", md: "row" }} justifyContent="space-between" pt={2}>
       <Box sx={{ display: { xs: "flex", md: "none" }, justifyContent: "end" }}>
-        <Box onClick={() => setMobileOpen(!mobileOpen)} aria-label="open-nav">
+        <Box onClick={() => setMobileOpen(!mobileOpen)}>
           {mobileOpen ? <CloseIcon width={24} height={24} /> : <MenuIcon width={24} height={24} />}
         </Box>
       </Box>
