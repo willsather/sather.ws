fbd04b72037f69753384aa3464dc564f25f3789e
Will Sather <satherw@vmware.com>
2023-10-31 08:55:31 -0700

    feat: add nextjs unit test blog post

--- src/app/api/route.test.ts
+++ src/app/api/route.test.ts
@@ -3,26 +3,23 @@
  */
 
 import { GET, POST } from "@/src/app/api/route";
-import { NextResponse } from "next/server";
 
 describe("api", () => {
-  const jsonSpy = jest.spyOn(NextResponse, "json");
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
   describe("GET", () => {
     it("retrieves", async () => {
       const response = await GET({ method: "GET" } as Request);
-      expect(jsonSpy).toHaveBeenCalledWith("Hello World", { status: 200 });
+
+      expect(response.ok).toBeTruthy();
+      expect(await response.json()).toEqual("Hello World");
     });
   });
 
   describe("POST", () => {
     it("posts", async () => {
-      await POST({ method: "POST" } as Request);
-      expect(jsonSpy).toHaveBeenCalledWith("Hello World - POST", { status: 201 });
+      const response = await POST({ method: "POST" } as Request);
+
+      expect(response.ok).toBeTruthy();
+      expect(await response.json()).toEqual("Hello World - POST");
     });
   });
 });
