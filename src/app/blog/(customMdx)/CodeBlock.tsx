5571a047af5ac9f4a3eb660882499752a3ca4203
Will Sather <satherw@vmware.com>
2024-04-18 17:00:16 -0400

    chore: fix filename overflow

--- src/app/blog/(customMdx)/CodeBlock.tsx
+++ src/app/blog/(customMdx)/CodeBlock.tsx
@@ -23,49 +23,58 @@ const CodeBlock: FC<CodeBlockProps> = ({
     <Highlight code={children} language={codeLanguage} theme={prismTheme}>
       {({ tokens, getLineProps, getTokenProps }) => (
         <>
-          <pre className="bg-gray-100 border-2 border-gray-500 my-4 rounded-lg font-mono">
-            {/*Only show header if filename exists OR language isn't text*/}
-            {(fileName != null || codeLanguage != "text") && (
-              <div className="mb-4 bg-gray-300 py-2 px-4 rounded-t-md">
-                <div
-                  className={`flex items-center ${
-                    fileName ? "justify-between" : "justify-end"
-                  }`}
-                >
-                  {fileName && (
-                    <p className="font-mono font-bold text-gray-500">
-                      {fileName}
-                    </p>
-                  )}
-
-                  <p className="font-mono text-blue-400 bg-blue-100 text-sm p-1 px-2 border-2 border-blue-400 rounded-lg">
-                    {codeLanguage.toUpperCase()}
-                  </p>
-                </div>
-              </div>
-            )}
-
-            {/*Code Block*/}
-            <div className="mt-2 mb-2 overflow-auto scrollbar-custom mx-4">
-              <div>
-                {tokens.map((line, i) => (
-                  <div key={i} {...getLineProps({ line })}>
-                    {!hideLineNumbers && (
-                      <span className="font-mono text-gray-400">{i + 1} </span>
+          <pre className="border-2 border-gray-500 my-4 rounded-lg font-mono">
+            <div className="bg-gray-300 rounded-t-md">
+              {/*Only show header if filename exists OR language isn't text*/}
+              {(fileName != null || codeLanguage != "text") && (
+                <div className="py-2 pl-4 mr-4 overflow-x-auto">
+                  <div
+                    className={`flex items-center ${
+                      fileName ? "justify-between" : "justify-end"
+                    }`}
+                  >
+                    {fileName && (
+                      <p className="font-mono font-bold text-gray-500 mr-4">
+                        {fileName}
+                      </p>
                     )}
 
-                    {(codeLanguage == "shell" ||
-                      codeLanguage == "bash" ||
-                      codeLanguage == "zsh") &&
-                      i < tokens.length - 1 && (
-                        <span className="font-mono text-pink-500 mr-2">$</span>
+                    <p className="font-mono text-blue-400 bg-blue-100 text-sm p-1 px-2 border-2 border-blue-400 rounded-lg">
+                      {codeLanguage.toUpperCase()}
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            {/*Code Block*/}
+            <div className="bg-gray-100 rounded-b-md">
+              <div className="p-2 px-4 overflow-auto scrollbar-custom">
+                <div>
+                  {tokens.map((line, i) => (
+                    <div key={i} {...getLineProps({ line })}>
+                      {!hideLineNumbers && (
+                        <span className="font-mono text-gray-400">
+                          {i + 1}{" "}
+                        </span>
                       )}
 
-                    {line.map((token, key) => (
-                      <span key={key} {...getTokenProps({ token })} />
-                    ))}
-                  </div>
-                ))}
+                      {/*Add Dollar Sign to Terminal*/}
+                      {(codeLanguage == "shell" ||
+                        codeLanguage == "bash" ||
+                        codeLanguage == "zsh") &&
+                        i < tokens.length - 1 && (
+                          <span className="font-mono text-pink-500 mr-2">
+                            $
+                          </span>
+                        )}
+
+                      {line.map((token, key) => (
+                        <span key={key} {...getTokenProps({ token })} />
+                      ))}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </pre>
