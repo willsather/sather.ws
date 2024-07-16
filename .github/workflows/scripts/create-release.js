777253b5b4a41b82a574f1936d08a1d53844aaeb
Will Sather <satherw@vmware.com>
2024-07-16 09:40:58 -0400

    chore: simplify ci

--- .github/workflows/scripts/create-release.js
+++ .github/workflows/scripts/create-release.js
@@ -1,45 +1,19 @@
+// @ts-check
+/** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ github, context, core }) => {
-  console.log("Release Type: ", process.env.RELEASE_TYPE);
-
-  const latestRelease = await github.rest.repos.getLatestRelease({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-  });
-
-  const latestTag = latestRelease.data.tag_name;
-  const versionParts = latestTag.replace(/^v/, "").split(".");
-
-  console.log("Latest Release Tag: ", latestTag);
-
-  let major = Number.parseInt(versionParts[0]);
-  let minor = Number.parseInt(versionParts[1]);
-  let patch = Number.parseInt(versionParts[2]);
-
-  if (process.env.RELEASE_TYPE === "major") {
-    major += 1;
-    minor = 0;
-    patch = 0;
-  } else if (process.env.RELEASE_TYPE === "minor") {
-    minor += 1;
-    patch = 0;
-  } else {
-    patch += 1;
-  }
-
-  const nextVersion = `v${major}.${minor}.${patch}`;
-
-  console.log("Creating Release: ", nextVersion);
+  const releaseName = context.sha.substring(0, 7);
+  console.log("Release Name: ", releaseName);
 
   const response = await github.rest.repos.createRelease({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    tag_name: nextVersion,
-    name: nextVersion,
+    tag_name: `release-${releaseName}`,
+    name: `Release ${releaseName}`,
     generate_release_notes: true,
     draft: false,
     prerelease: false,
   });
 
-  core.setOutput("version", nextVersion);
+  console.log("Release URL: ", response.data.html_url);
   core.setOutput("release_url", response.data.html_url);
 };
