d4bdb4da69a1ebac493a89ef37727fdf3abf7814
Will Sather <satherw@vmware.com>
2024-07-25 16:18:36 -0400

    fix: component padding

--- src/app/blog/(customMdx)/Component.tsx
+++ src/app/blog/(customMdx)/Component.tsx
@@ -1,6 +1,6 @@
 export default function Component({ name }: { name: string }) {
   return (
-    <code className="rounded-md border-2 border-gray-500 px-1 py-0.5 text-gray-500 text-sm">
+    <code className="rounded-md border-2 border-gray-500 px-1 text-gray-500 text-sm">
       {"<"}
       {name}
       {"/>"}
