2c44941b00995fa9660ebfaea99f70a50bb50e65
willsather <will@sather.ws>
2025-03-20 14:06:44 -0400

    chore: small work fix

--- src/app/work/job-list.tsx
+++ src/app/work/job-list.tsx
@@ -37,7 +37,7 @@ export default function JobList() {
         {jobs.map((job) => (
           <div className="my-8 md:my-12" key={job.company}>
             <div className="flex items-center">
-              <h3 className="text-3xl md:text-5xl">{job.company}.</h3>
+              <h3 className="text-3xl md:text-5xl">{job.company}</h3>
 
               {job?.tag != null && (
                 <div className="ml-4 rounded-lg bg-amber-500 p-1 px-2">
@@ -45,8 +45,8 @@ export default function JobList() {
                 </div>
               )}
             </div>
-            <h4 className="text-xl md:text-2xl">{job.role}.</h4>
-            <p className="font-mono text-gray-500">{job.description}.</p>
+            <h4 className="text-xl md:text-2xl">{job.role}</h4>
+            <p className="font-mono text-gray-500">{job.description}</p>
           </div>
         ))}
       </ul>
