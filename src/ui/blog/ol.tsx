4ab1d49d0bb45c5cc9dd0d43abbda13d36663cab
willsather <will@sather.ws>
2025-10-15 23:45:47 -0500

    fix: list components

--- src/ui/blog/ol.tsx
+++ src/ui/blog/ol.tsx
@@ -1,5 +1,10 @@
 import type { OlHTMLAttributes } from "react";
 
 export default function OL({ ...props }: OlHTMLAttributes<HTMLOListElement>) {
-  return <ol className="mb-4 ml-6 list-decimal" {...props} />;
+  return (
+    <ol
+      className="mb-4 space-y-2 text-gray-300 list-decimal pl-6 [&>li]:ml-[-0.5rem]"
+      {...props}
+    />
+  );
 }
--- src/ui/blog/ul.tsx
+++ src/ui/blog/ul.tsx
@@ -1,5 +1,10 @@
 import type { HTMLAttributes } from "react";
 
 export default function UL({ ...props }: HTMLAttributes<HTMLUListElement>) {
-  return <ul className="mb-4 space-y-2 text-gray-300" {...props} />;
+  return (
+    <ul
+      className="mb-4 space-y-2 text-gray-300 list-disc pl-6 [&>li]:ml-[-0.5rem]"
+      {...props}
+    />
+  );
 }
