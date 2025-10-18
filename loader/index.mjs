ca26f6f781ab5b74e51bf13851c19e7752bcdb16
willsather <will@sather.ws>
2025-10-18 16:44:40 -0500

    feat: add turbo mdx loader

---
+++ loader/index.mjs
@@ -0,0 +1,63 @@
+import * as mdx from "@mdx-js/mdx";
+import rehypeShiki from "@shikijs/rehype";
+import {
+  transformerNotationDiff,
+  transformerNotationHighlight,
+} from "@shikijs/transformers";
+import rehypeMdxCodeProps from "rehype-mdx-code-props";
+import rehypeSlug from "rehype-slug";
+
+const DEFAULT_RENDERER = `import React from 'react'`;
+
+// custom shiki configuration matching next.config.mjs
+const rehypeShikiOptions = {
+  themes: { light: "one-dark-pro", dark: "one-dark-pro" },
+  addLanguageClass: true,
+  transformers: [transformerNotationDiff(), transformerNotationHighlight()],
+
+  // hack to pass data props through shiki
+  // https://github.com/shikijs/shiki/issues/629
+  parseMetaString: (str) => {
+    return Object.fromEntries(
+      str.split(" ").reduce((prev, curr) => {
+        const [key, value] = curr.split("=");
+        const isNormalKey = /^[A-Z0-9]+$/i.test(key);
+        if (isNormalKey) {
+          prev.push([key, value || true]);
+        }
+        return prev;
+      }, []),
+    );
+  },
+};
+
+const loader = async function (content) {
+  const callback = this.async();
+  const isDev = this.mode === "development";
+
+  const options = {
+    development: isDev,
+    providerImportSource: "@/mdx-components",
+    remarkPlugins: [],
+    rehypePlugins: [
+      [rehypeShiki, rehypeShikiOptions],
+      rehypeSlug, // add `id` to all headings
+      rehypeMdxCodeProps, // provide custom props on `code` and `pre` blocks
+    ],
+  };
+
+  let result;
+
+  try {
+    result = await mdx.compile(content, options);
+  } catch (err) {
+    return callback(err);
+  }
+
+  const { renderer = DEFAULT_RENDERER } = options;
+
+  const code = `${renderer}\n${result}`;
+  return callback(null, code);
+};
+
+export default loader;
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
-import createMDX from "@next/mdx";
-import rehypeShiki from "@shikijs/rehype";
-import {
-  transformerNotationDiff,
-  transformerNotationHighlight,
-} from "@shikijs/transformers";
 import withVercelToolbar from "@vercel/toolbar/plugins/next";
-import rehypeMdxCodeProps from "rehype-mdx-code-props";
-import rehypeSlug from "rehype-slug";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  turbopack: {
+    rules: {
+      "*.mdx": {
+        loaders: ["turbopack-mdx-loader"],
+        as: "*.tsx",
+      },
+    },
+  },
 
   // temporary redirect for any old, lingering urls
   async redirects() {
@@ -39,43 +39,6 @@ const nextConfig = {
   },
 };
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [
-      [
-        rehypeShiki,
-        {
-          themes: { light: "one-dark-pro", dark: "one-dark-pro" },
-          addLanguageClass: true,
-          transformers: [
-            transformerNotationDiff(),
-            transformerNotationHighlight(),
-          ],
-
-          // hack to pass data props through shiki
-          // https://github.com/shikijs/shiki/issues/629
-          parseMetaString: (str) => {
-            return Object.fromEntries(
-              str.split(" ").reduce((prev, curr) => {
-                const [key, value] = curr.split("=");
-                const isNormalKey = /^[A-Z0-9]+$/i.test(key);
-                if (isNormalKey) {
-                  prev.push([key, value || true]);
-                }
-                return prev;
-              }, []),
-            );
-          },
-        },
-      ],
-      rehypeSlug, // add `id` to all headings
-      rehypeMdxCodeProps, // provide custom props on `code` and `pre` blocks
-    ],
-  },
-});
-
-const configWithMDX = withMDX(nextConfig);
-const configWithVercelToolbar = withVercelToolbar()(configWithMDX);
+const configWithVercelToolbar = withVercelToolbar()(nextConfig);
 
 export default configWithVercelToolbar;
